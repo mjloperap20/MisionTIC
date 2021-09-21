c9f6d3dd7b1c1da33fea619393adbf11ccc65667
mjloperap20 <mlopera@sura.com.co>
2021-09-20 19:43:29 -0500

    First Commit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Estaciones.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ProyectoCiclo3.App.Dominio
+{
+     public class Estaciones{
+         public int id {get;set;}
+         public string nombre {get;set;}
+         public string direccion {get;set;}
+         public float coordx {get;set;}
+         public float coordy {get;set;}
+         public string tipo {set;get;}
+     }
+}
